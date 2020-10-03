77e410050bf64c9bf2464a4aa4e67f987f1497dd
san-kir-k <dfomka@list.ru>
2020-10-03 21:45:30 +0300

    Добавлена генерация невалидных данных, теперь проверка деления на ноль только в 5-м тесте

--- test/generator.py
+++ test/generator.py
@@ -9,20 +9,40 @@ def main():
 
     test_dir = sys.argv[1]
 
-    line_count = [1, 5, 10, 20, 5]
+    line_count = [5, 5, 10, 20, 5]
     for test_count in range(5):
-        name_len = random.randint(1, 5)
-        name = "file_" + "".join([random.choice(string.ascii_letters) for _ in range(name_len)])
+        name = "file_" + str(test_count + 1)
 
         test = []
 
-        for _ in range(line_count[test_count]):
-            val_list = [round(random.uniform(-5.0, 15.0), random.randint(0, 5)) for _ in range(3)]
-            test.append(val_list)
+        # тест с невалидными данными
+        if  test_count == 0:
+            for _ in range(line_count[test_count]):
+                val_list = [round(random.uniform(-10.0, 10.0), random.randint(0, 5)) for _ in range(3)]
+                for i in range(3):
+                    if val_list[i] == 0.0:
+                        val_list[i] = 1.0
+                test.append(val_list)
+            test[3][0] = "1.2.3"
+            test[4][2] = "Hello world!"
 
-        # сгенерируем случай с нулем
-        if test_count == 4:
-            test[2][1] = 0.0
+        # обычные числа > 0
+        if test_count == 1 or test_count == 2:
+            for _ in range(line_count[test_count]):
+                val_list = [round(random.uniform(1.0, 15.0), random.randint(0, 5)) for _ in range(3)]
+                test.append(val_list)
+        
+        # положительные и отрицательные числа без нуля
+        if test_count == 3 or test_count == 4:
+            for _ in range(line_count[test_count]):
+                val_list = [round(random.uniform(-10.0, 10.0), random.randint(0, 5)) for _ in range(3)]
+                for i in range(3):
+                    if val_list[i] == 0.0:
+                        val_list[i] = 1.0
+                test.append(val_list)
+            # сгенерируем случай с нулем
+            if test_count == 4:
+                test[2][1] = 0.0
 
         test_name = "{}/test_{}".format(test_dir, test_count + 1)
         with open(f'{test_name}.txt', 'w') as ftest:
